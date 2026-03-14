f91473bab7e90b2f9d4a9a237e78b100b18b9ea7
unknown <gustavosilvam85@gmail.com>
2026-03-14 01:07:16 -0300

    função de calculo implementada

--- lib/pages/calculadora_page.dart
+++ lib/pages/calculadora_page.dart
@@ -1,4 +1,5 @@
 import 'package:calculadora/enums/operation_type.dart';
+import 'package:calculadora/pages/historic_page.dart';
 import 'package:calculadora/widgets/buttom_widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -22,28 +23,102 @@ class _CalculadoraPageState extends State<CalculadoraPage> {
       displaynumber += newType.symbol;
     });
   }
-  void clearCalculator(){
+
+  void clearCalculator() {
     setState(() {
       displaynumber = "0";
     });
   }
-  void appendNumber(String stringNumber){
+
+  void appendNumber(String stringNumber) {
     setState(() {
-      if(displaynumber == "0"){
+      if (displaynumber == "0") {
         displaynumber = stringNumber;
-      }else{
+      } else {
         displaynumber += stringNumber;
       }
     });
   }
-  void removeNumber(){
+
+  void removeNumber() {
     setState(() {
-      if(displaynumber.length <=1){
+      if (displaynumber.length <= 1) {
         displaynumber = "0";
-      }else{
-        displaynumber = displaynumber.substring(0,displaynumber.length-1);
+      } else {
+        displaynumber = displaynumber.substring(0, displaynumber.length - 1);
       }
     });
+  }
+
+  List<double> parseNumber(String expression) {
+    RegExp regExp = RegExp('[0-9]+\.?[0-9]*');
+
+    var matches = regExp.allMatches(expression);
+
+    List<double> numbers = [];
+
+    for (var match in matches) {
+      String numberText = match.group(0)!;
+
+      numbers.add(double.parse(numberText));
+    }
+    return numbers;
+  }
+
+  List<OperationTypeEnum> getOperator(String expression) {
+    final expression1 = expression.characters.where(
+      (x) => OperationTypeEnum.values.any((op) => op.symbol == x),
+    );
+
+    return expression1.map((x) => OperationTypeEnum.values.firstWhere((op)=> op.symbol == x)).toList();
+  }
+
+  void calculate(){
+    String expression = displaynumber.replaceAll('.', '.');
+    List<double> numbers = parseNumber(expression);
+    List<OperationTypeEnum> operations = getOperator(expression);
+
+    resolvePriorityOperations(numbers, operations);
+    final result = resolveAdditionAndSubtraction(numbers, operations);
+
+    setState((){
+      displaynumber = result.toString().replaceAll('.', ',');
+    });
+  }
+
+  void resolvePriorityOperations(List<double> numbers, List<OperationTypeEnum> operators){
+    int index = 0;
+
+    while(index < operators.length){
+      if(operators[index] == OperationTypeEnum.multiplication){
+        numbers[index] = numbers[index] * numbers[index +1];
+        numbers.removeAt(index+1);
+        operators.removeAt(index);
+      } else if(operators[index] == OperationTypeEnum.division){
+        numbers[index] = numbers[index] / numbers[index +1];
+        numbers.removeAt(index+1);
+        operators.removeAt(index);
+      }else{
+        index++;
+      }
+    }
+  }
+
+  double resolveAdditionAndSubtraction(List<double> numbers, List<OperationTypeEnum> operators){
+    int index = 0;
+
+    while(index < operators.length){
+      if(operators[index] == OperationTypeEnum.addition){
+        numbers[index] = numbers[index] + numbers[index +1];
+        numbers.removeAt(index+1);
+        operators.removeAt(index);
+      } else if(operators[index] == OperationTypeEnum.subtraction){
+        numbers[index] = numbers[index] - numbers[index +1];
+        numbers.removeAt(index+1);
+        operators.removeAt(index);
+      }
+    }
+    return numbers[0];
   }
   @override
   Widget build(BuildContext context) {
@@ -52,6 +127,12 @@ class _CalculadoraPageState extends State<CalculadoraPage> {
         title: Text("Calculadora"),
         backgroundColor: Colors.lightBlue,
         foregroundColor: Colors.white,
+        actions: [
+          IconButton(
+            onPressed: (){Navigator.push(context, MaterialPageRoute(builder: ()=>HistoricPage(historic: historic));
+          );};
+          icon: Icon(Icons.history))
+        ],
       ),
       body: Column(
         children: [
@@ -72,10 +153,18 @@ class _CalculadoraPageState extends State<CalculadoraPage> {
             children: [
               Row(
                 children: [
-                  ButtomWidgets(text: "C", onPressed: () {clearCalculator();}, color: Colors.red),
+                  ButtomWidgets(
+                    text: "C",
+                    onPressed: () {
+                      clearCalculator();
+                    },
+                    color: Colors.red,
+                  ),
                   ButtomWidgets(
                     text: "⌫",
-                    onPressed: () {removeNumber();},
+                    onPressed: () {
+                      removeNumber();
+                    },
                     color: Colors.orangeAccent,
                   ),
                   ButtomWidgets(
@@ -89,9 +178,24 @@ class _CalculadoraPageState extends State<CalculadoraPage> {
               ),
               Row(
                 children: [
-                  ButtomWidgets(text: "7", onPressed: () {appendNumber('7');}),
-                  ButtomWidgets(text: "8", onPressed: () {appendNumber('8');}),
-                  ButtomWidgets(text: "9", onPressed: () {appendNumber('9');}),
+                  ButtomWidgets(
+                    text: "7",
+                    onPressed: () {
+                      appendNumber('7');
+                    },
+                  ),
+                  ButtomWidgets(
+                    text: "8",
+                    onPressed: () {
+                      appendNumber('8');
+                    },
+                  ),
+                  ButtomWidgets(
+                    text: "9",
+                    onPressed: () {
+                      appendNumber('9');
+                    },
+                  ),
                   ButtomWidgets(
                     text: "X",
                     onPressed: () {
@@ -104,9 +208,24 @@ class _CalculadoraPageState extends State<CalculadoraPage> {
               ),
               Row(
                 children: [
-                  ButtomWidgets(text: "4", onPressed: () {appendNumber('4');}),
-                  ButtomWidgets(text: "5", onPressed: () {appendNumber('5');}),
-                  ButtomWidgets(text: "6", onPressed: () {appendNumber('6');}),
+                  ButtomWidgets(
+                    text: "4",
+                    onPressed: () {
+                      appendNumber('4');
+                    },
+                  ),
+                  ButtomWidgets(
+                    text: "5",
+                    onPressed: () {
+                      appendNumber('5');
+                    },
+                  ),
+                  ButtomWidgets(
+                    text: "6",
+                    onPressed: () {
+                      appendNumber('6');
+                    },
+                  ),
                   ButtomWidgets(
                     text: "-",
                     onPressed: () {
@@ -119,9 +238,24 @@ class _CalculadoraPageState extends State<CalculadoraPage> {
               ),
               Row(
                 children: [
-                  ButtomWidgets(text: "1", onPressed: () {appendNumber('1');}),
-                  ButtomWidgets(text: "2", onPressed: () {appendNumber('2');}),
-                  ButtomWidgets(text: "3", onPressed: () {appendNumber('3');}),
+                  ButtomWidgets(
+                    text: "1",
+                    onPressed: () {
+                      appendNumber('1');
+                    },
+                  ),
+                  ButtomWidgets(
+                    text: "2",
+                    onPressed: () {
+                      appendNumber('2');
+                    },
+                  ),
+                  ButtomWidgets(
+                    text: "3",
+                    onPressed: () {
+                      appendNumber('3');
+                    },
+                  ),
                   ButtomWidgets(
                     text: "+",
                     onPressed: () {
@@ -134,11 +268,23 @@ class _CalculadoraPageState extends State<CalculadoraPage> {
               ),
               Row(
                 children: [
-                  ButtomWidgets(text: "0", onPressed: () {appendNumber('0');}),
-                  ButtomWidgets(text: ",", onPressed: () {appendNumber(',');}),
+                  ButtomWidgets(
+                    text: "0",
+                    onPressed: () {
+                      appendNumber('0');
+                    },
+                  ),
+                  ButtomWidgets(
+                    text: ",",
+                    onPressed: () {
+                      appendNumber(',');
+                    },
+                  ),
                   ButtomWidgets(
                     text: "=",
-                    onPressed: () {},
+                    onPressed: () {
+                      calculate();
+                    },
                     color: Colors.green,
                   ),
                 ],
---
+++ lib/pages/historic_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HistoricPage extends StatefulWidget {
+  const HistoricPage({super.key});
+
+  @override
+  State<HistoricPage> createState() => _HistoricPageState();
+}
+
+class _HistoricPageState extends State<HistoricPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Historico"),
+        backgroundColor: Colors.pinkAccent,
+      ),
+    );
+  }
+}
